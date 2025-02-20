6a78c5ffb069a417ef0236666ecbefe0952dd8d7
charaf <kaouri.charafeddine@gmail.com>
2025-02-20 09:04:47 +0100

    Set up database connection and update styles for Tailwind CSS integration

--- src/database/connectDB.php
+++ src/database/connectDB.php
@@ -1,1 +1,12 @@
+<?php
+$host = "localhost:88";
+$user = "root"; 
+$pass = ""; 
+$dbname = "efoot";
 
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
--- src/view/index.php
+++ src/view/index.php
@@ -3,10 +3,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="./output.css" rel="stylesheet">
+  <link href="./styles/output.css" rel="stylesheet">
 </head>
 <body>
-  <h1 class="text-3xl font-bold underline">
+  <h1 class=" bg-amber-800 ">
     Hello world!
   </h1>
 </body>
