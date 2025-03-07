b23649e5fd30d28d8b7e2cb8105c65792c283644
charaf <kaouri.charafeddine@gmail.com>
2025-03-07 06:02:42 +0000

    Add dotenv support and environment configuration; remove unused club styles

--- src/database/connectDB.php
+++ src/database/connectDB.php
@@ -1,31 +1,39 @@
 <?php
 
-trait DbConnection{
+require_once __DIR__ . '/../../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); 
+$dotenv->load();
+var_dump($_ENV);
 
-    static private $host = "localhost:88";
-    static private $user = "root"; 
-    static private $pass = ""; 
-    static private $dbname = "efoot";
-    static private $charset = "utf8mb4";
+trait DbConnection
+{
 
-    // static private $host = "efoot-ilisi1-2025-marwanmoujahid008-82ae.h.aivencloud.com:19232";
-    // static private $user = "avnadmin"; 
-   
-    // static private $dbname = "efoot";
-    // static private $charset = "utf8mb4";
+    static private $host;
+    static private $user;
+    static private $pass;
+    static private $dbname;
+    static private $charset;
 
-    public static function connect(){
+
+
+    public static function connect()
+    {
+
+        self::$host = $_ENV['DB_HOST'];
+        self::$user = $_ENV['DB_USER'];
+        self::$pass = $_ENV['DB_PASS'];
+        self::$dbname = $_ENV['DB_NAME'];
+        self::$charset = $_ENV['DB_CHARSET'];
         try {
-            $dsn = "mysql:host=".self::$host.";dbname=".self::$dbname.";charset=".self::$charset;
+            $dsn = "mysql:host=" . self::$host . ";dbname=" . self::$dbname . ";charset=" . self::$charset;
             $pdo = new PDO($dsn, self::$user, self::$pass);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $pdo;
         } catch (PDOException $e) {
-            echo "Connection failed: ".$e->getMessage();
+            echo "Connection failed: " . $e->getMessage();
         }
     }
-
-
 }
+
 
